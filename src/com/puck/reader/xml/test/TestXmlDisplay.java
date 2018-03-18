1a23c8d9a7e0dc3dd6d1c60352417575f428d019
CharafLachachi <lac.charaf@gmail.com>
2018-03-18 19:16:47 +0100

    DisplayGrpah ----> Xml

--- src/com/puck/reader/xml/test/TestXmlDisplay.java
+++ src/com/puck/reader/xml/test/TestXmlDisplay.java
@@ -26,20 +26,21 @@ import com.puck.utilities.piccolo2d.XmlToStructure;
  * 
  * */
 public class TestXmlDisplay {
-	private StringBuilder xmlString = new  StringBuilder();
+	private StringBuilder xmlString = new StringBuilder();
 	private PiccoloCustomNode root;
-	private  HashMap<String, Edge> addedEdges = new HashMap<String, Edge>();
-	private  HashMap<String, PiccoloCustomNode> addedPnodes = new HashMap<String, PiccoloCustomNode>();
+	private HashMap<String, Edge> addedEdges = new HashMap<String, Edge>();
+	private HashMap<String, PiccoloCustomNode> addedPnodes = new HashMap<String, PiccoloCustomNode>();
 	private HashMap<String, PiccoloCustomNode> allPNodes = new HashMap<>();
 	private Map<String, Node> listNodes;
-	private BufferedWriter br ;
+	private BufferedWriter br;
 	private CreateEgdesHierarchyBy createEdgesHierarchyBy;
 	private CreateEgdesHierarchyOf createEdgesHierarchyOf;
 	private PSwingCanvas canvas;
 	private Menu menu;
 	private ArrowNodesHolder ANH;
+
 	public TestXmlDisplay() {
-		this.listNodes =new XmlToStructure("mongraph.xml").parseNode();
+		this.listNodes = new XmlToStructure("mongraph.xml").parseNode();
 		this.root = new NodeToPnodeParser(allPNodes, listNodes).getPackageNodes();
 		this.canvas = new PSwingCanvas();
 		this.menu = new Menu();
@@ -52,12 +53,12 @@ public class TestXmlDisplay {
 		createEdgesHierarchyBy.drawOutgoingdges(root, canvas);
 		createEdgesHierarchyOf.drawOutgoingdges(root, canvas);
 		System.err.println(ANH.getAllArrows().size());
-		 for (Entry<String, PiccoloCustomNode> entry : addedPnodes.entrySet()) {
-				String key = entry.getKey();
-				PiccoloCustomNode p = entry.getValue();
-				addEdgesToXml(p);
-		 }
-		
+		for (Entry<String, PiccoloCustomNode> entry : addedPnodes.entrySet()) {
+			String key = entry.getKey();
+			PiccoloCustomNode p = entry.getValue();
+			addEdgesToXml(p);
+		}
+
 		try {
 			writeTo();
 		} catch (IOException e) {
@@ -65,73 +66,75 @@ public class TestXmlDisplay {
 			e.printStackTrace();
 		}
 	}
+
 	public void addPnodes(PiccoloCustomNode pnode) {
 		for (PiccoloCustomNode child : pnode.getHierarchy()) {
 			addPnodeToXml(child);
 			addPnodes(child);
 		}
-		
+
 	}
-	
+
 	public void addPnodeToXml(PiccoloCustomNode pnode) {
 		if (!addedPnodes.containsValue(pnode)) {
-			System.out.println(pnode.getName());	
+			System.out.println(pnode.getName());
 			System.out.println(pnode.isHidden());
 			addedPnodes.put(pnode.getidNode(), pnode);
 			xmlString.append(nodeToString(pnode));
 		}
 	}
-	
-	 private String nodeToString(PiccoloCustomNode node) {
-	        String formatString = "\t<node type=\"%s\" id=\"%s\" name=\"%s\"/>\n";
-	        String type = node.getContent().getType().toLowerCase();
-	        String id = node.getidNode();
-	        String name = node.getName();
-	        return String.format(formatString, type, id, name);
-	    }
-	 private String edgeToString(Edge node) {
-	        String formatString = "\t<edge type=\"%s\" id=\"%s\" src=\"%s\" dest=\"%s\"/>\n";
-	        String type = node.getType();
-	        String id = node.getId();
-	        String from = node.getFrom();
-	        String to = node.getTo();
-	        return String.format(formatString, type, id, from,to);
-	    }
-	
-	 public void writeTo() throws IOException {
-			BufferedWriter br = new BufferedWriter(new FileWriter(new File("xmlFromDisplay.xml")));
-	        br.write(xmlString.toString());
-	        br.close();
-	    }
-	 public void addEdgesToXml(PiccoloCustomNode pnode) {
-		 Node node = listNodes.get(pnode.getidNode());
-		 for (Entry<String, PiccoloCustomNode> entry : addedPnodes.entrySet()) {
-				String key = entry.getKey();
-				PiccoloCustomNode p = entry.getValue();
-				for (PiccoloCustomNode child : p.getChildren()) {
-					if(node != null) {
+
+	private String nodeToString(PiccoloCustomNode node) {
+		String formatString = "\t<node type=\"%s\" id=\"%s\" name=\"%s\"/>\n";
+		String type = node.getContent().getType().toLowerCase();
+		String id = node.getidNode();
+		String name = node.getName();
+		return String.format(formatString, type, id, name);
+	}
+
+	private String edgeToString(Edge node) {
+		String formatString = "\t<edge type=\"%s\" id=\"%s\" src=\"%s\" dest=\"%s\"/>\n";
+		String type = node.getType();
+		String id = node.getId();
+		String from = node.getFrom();
+		String to = node.getTo();
+		return String.format(formatString, type, id, from, to);
+	}
+
+	public void writeTo() throws IOException {
+		BufferedWriter br = new BufferedWriter(new FileWriter(new File("xmlFromDisplay.xml")));
+		br.write(xmlString.toString());
+		br.close();
+	}
+
+	public void addEdgesToXml(PiccoloCustomNode pnode) {
+		Node node = listNodes.get(pnode.getidNode());
+		for (Entry<String, PiccoloCustomNode> entry : addedPnodes.entrySet()) {
+			String key = entry.getKey();
+			PiccoloCustomNode p = entry.getValue();
+			for (PiccoloCustomNode child : p.getChildren()) {
+				if (node != null) {
 					Edge e = getCointainsEdge(node, listNodes.get(child.getidNode()));
 					if (e != null && !addedEdges.containsKey(e)) {
 						xmlString.append(edgeToString(e));
 					}
-					}
-					//addEdgesToXml(child);
 				}
-				
-		 }
-		 
-	 }
-	 public Edge getCointainsEdge(Node node,Node child) {
-		 HashMap<String, Edge> edges = node.getRelation();
-		 for (Entry<String, Edge> entry : edges.entrySet()) {
-				Edge e = entry.getValue();
-				if (e.getType().equals("contains") && e.getTo().equals(child.getId())) {
-				return e;	
-				}
-		 }
-		 return null;
-	 }
-	 public static void main(String[] args) {
+			}
+		}
+	}
+
+	public Edge getCointainsEdge(Node node, Node child) {
+		HashMap<String, Edge> edges = node.getRelation();
+		for (Entry<String, Edge> entry : edges.entrySet()) {
+			Edge e = entry.getValue();
+			if (e.getType().equals("contains") && e.getTo().equals(child.getId())) {
+				return e;
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
 		new TestXmlDisplay();
 	}
 }
